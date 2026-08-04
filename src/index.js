cd5f1051e64088ed4f31e8249237fc38ba31e416
Ontibile-ai <ontmmusi@gmail.com>
2026-08-04 21:27:25 +0200

    js layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["Loading a Poem for You..."],
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
