df0fe6bd6a24c1f7d1d30d9b23a64f8dd106d4fa
Ontibile-ai <ontmmusi@gmail.com>
2026-08-06 21:09:00 +0200

    finalized

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ function generatePoem(event) {
   let apiKey = "cbab4a1ta01fc9fe9f10bf357f41o145";
   let prompt = `Generate a poem about ${inputElement.value}`;
   let context =
-    "you are a helpful assistant. let the poem be short and sweet, with a touch of whimsy and emotion. Make sure to limit the poem to 6 lines in basic html and separate them with <br> tags. Wrap the lines with <li> tags. Justify the lines. Sign the poem with <strong>With love, Ontibile.</strong>";
+    "you are a helpful assistant. let the poem be short and sweet, with a touch of whimsy and emotion. Make sure to limit the poem to 6 lines in basic html and separate them with <br> tags. Wrap the lines with <li> tags. Justify the lines. Sign the poem with <strong>Come Away with Me, Ontibile.</strong>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink"> Generating poem about ${inputElement.value}...</div>`;
+  poemElement.innerHTML = `<div class="blink"> Generating a poem about ${inputElement.value}...</div>`;
 
   console.log(`generatePoem called with input: ${inputElement.value}`);
   console.log(`Prompt: ${prompt}`);
